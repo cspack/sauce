b36d09dcd1408ecc929904a39bc361f07503546e
cspack <spackjlefish@gmail.com>
2024-08-31 20:22:56 -0700

    Update BotRunner_copy_20240831181543.ino
    Hall effect scanner.

--- BotRunner_copy_20240831181543/BotRunner_copy_20240831181543.ino
+++ BotRunner_copy_20240831181543/BotRunner_copy_20240831181543.ino
@@ -1,29 +1,45 @@
+// IO CONFIGURATION
+const int hallSensorPin = 3;
+const int hallLedPin = 4;
+
 // How many rotations to hold onto in the motor.
 const int ROTATIONS_COLLECTED = 100;
+uint64_t lastTime = 0;
 
 struct Wheel {
-  uint32_t ticks[ROTATIONS_COLLECTED];
+  uint64_t ticks[ROTATIONS_COLLECTED];
   int index = 0;
+  int tickCount = 0;
 
   void recordTick(void) {
-    int next = index++ % ROTATIONS_COLLECTED;
-    ticks[next] = millis();
-    index = next;
+    tickCount++;
+    ticks[index] = millis();
+    index = (++index) % ROTATIONS_COLLECTED;
   }
 
-  int countTicks(uint32_t time, int max_ms) {
+  int countTicks(uint64_t now, uint64_t window) {
     int count = 0;
-    int i = index;
-    int now = time;
-    int min_time = now - max_ms;
+
+    // Circuluar access to last node.
+    int i = index-1;
+    if (i < 0) i += ROTATIONS_COLLECTED;
+
+    uint64_t min_time = now - window;
+    if (min_time < 0)
+      return 0;
+
     while (true) {
-      int next = i--;
-      // Modulus operator might make this simpler, but i'm not going to overthink.
-      if (next < 0) next += ROTATIONS_COLLECTED;
-      i = next;
-      if (ticks[i] != -1 && ticks[i] >= min_time) {
+      if (ticks[i] >= min_time) {
         count++;
+        if (count > ROTATIONS_COLLECTED) {
+          // FUCK!
+          break;
+        }
+
+        i--;
+        if (i < 0) i += ROTATIONS_COLLECTED;
       } else {
+        // Expected.
         break;
       }
     }
@@ -31,45 +47,59 @@ struct Wheel {
   }
 };
 
-
-static Wheel
-initWheel() {
-  Wheel wheel;
+void initWheel(Wheel wheel) {
+  wheel.index = 0;
+  wheel.tickCount = 0;
   for (int i = 0; i < sizeof(wheel.ticks); i++) wheel.ticks[i] = -1;
-  return wheel;
 }
-Wheel left = initWheel();
-Wheel right = initWheel();
+Wheel left;
+Wheel right;
 
 void leftInterrupt() {
   left.recordTick();
 }
+
 void rightInterrupt() {
   right.recordTick();
 }
 
 void hallInterrupt() {
-  Serial.println("BYE");
 }
 
-const int hallSensorPin = 3;
-
 void setup() {
+  initWheel(left);
+  initWheel(right);
+
   Serial.begin(9600);
+  Serial.println("Let's go!");
 
   // Set up the Hall effect sensor pin
   pinMode(hallSensorPin, INPUT);
+  pinMode(hallLedPin, OUTPUT);
 
-  attachInterrupt(A1, leftInterrupt, FALLING);
-  attachInterrupt(A2, rightInterrupt, FALLING);
-  attachInterrupt(hallSensorPin, hallInterrupt, FALLING);
+  // Zero out things.
+  digitalWrite(hallLedPin, HIGH);
 
+  attachInterrupt(digitalPinToInterrupt(hallSensorPin), leftInterrupt, FALLING);
+}
+
+void printUint64(uint64_t value) {
+  uint32_t high = value >> 32;
+  uint32_t low = value & 0xFFFFFFFF;
+  Serial.print(high, HEX);
+  Serial.print(low, HEX);
 }
 
 void loop() {
-  uint32_t now = millis();
-  Serial.print("[1s] left ticks: ");
-  Serial.print(left.countTicks(now, 1000));
-  Serial.print(" right ticks: ");
-  Serial.println(right.countTicks(now, 1000));
+  uint64_t now = millis();
+  if ((now - lastTime) >= 1000) {
+    Serial.print("[1s @");
+    printUint64(now);
+    Serial.print("] left ticks: ");
+    //Serial.println(left.tickCount);
+    Serial.print(left.countTicks(now, 1000));
+    Serial.print(" right ticks: ");
+    Serial.println(right.countTicks(now, 1000));
+    lastTime = now;
+  }
 }
